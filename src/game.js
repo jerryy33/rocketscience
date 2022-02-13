67b5da26aea288f19c2a60632c14fe0de9ee620c
Jeremy <jeremy.herbst111@gmail.com>
2022-02-13 20:04:50 +0100

    improved webpack bundling and deleted unnecessary images

--- src/game.js
+++ src/game.js
@@ -1,50 +1,51 @@
 import Phaser from 'phaser';
+import './style.css';
 
 const config = {
     type: Phaser.AUTO,
     scale: {
-        width: document.body.clientWidth,
-        height: document.body.clientHeight,
+        width: window.innerWidth,
+        height: window.innerHeight,
     },
     physics: {
         default: 'arcade',
         arcade: {
             gravity: {y: 300},
-            debug: false
-        }
+            debug: false,
+        },
     },
     scene: {
         preload: preload,
         create: create,
-        update: update
-    }
+        update: update,
+    },
 };
 
 let asteroid;
 let asteroids;
 let game = new Phaser.Game(config);
 
+/**
+*/
 function preload() {
     this.load.image('universe', 'assets/universe.jpg');
-    this.load.image('ground', 'assets/platform.png');
-    this.load.image('asteroid', 'assets/asteroidv2.jpg');
+    this.load.image('asteroid', 'assets/asteroid.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-function create ()
-{
-  this.add.image(400, 300, 'universe');
-
-  asteroid = this.physics.add.image(100,100,'asteroid')
-  asteroid.setBounce(0.4);
-  asteroid.setCollideWorldBounds(true);
-  asteroids = this.physics.add.group();
-  asteroids.create('asteroidv2').setVelocity(100,0)
-
-
+/**
+*/
+function create() {
+    this.add.image(500, 1200, 'universe');
+    asteroid = this.physics.add.image(100, 100, 'asteroid')
+    asteroid.setBounce(0.4);
+    asteroid.setCollideWorldBounds(true);
+    asteroids = this.physics.add.group();
+    asteroids.create('asteroid').setVelocity(100, 0);
 }
 
+/**
+*/
 function update ()
 {
 }
