615641058332e63e26e01316425686bb2e8c48e0
Jeremy <jeremy.herbst111@gmail.com>
2022-04-27 21:20:05 +0200

    added tilesprite and camera for endless map mechanism

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -18,6 +18,7 @@ export default class Game extends Phaser.Scene {
     constructor() {
         super({key: 'mainGame'});
         this.canvas;
+        this.universe;
         this.asteroids;
         this.stars;
         this.rocket;
@@ -41,21 +42,24 @@ export default class Game extends Phaser.Scene {
      */
     create() {
         this.canvas = this.sys.game.canvas;
-
+        const atlasTexture = this.textures.get('rocket');
+        const frames = atlasTexture.getFrameNames();
+        console.log(frames);
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        const universe = this.add.image(0, 0, 'universe').setOrigin(0, 0);
+        this.universe = this.add.tileSprite(0, 0, this.canvas.width,
+            this.canvas.height, 'universe').setOrigin(0, 0).setScrollFactor(0);
+        console.log(this.universe.displayWidth);
+        this.rocket = new Rocket(this, 50, 300, 'rocket', frames[0]);
+        this.cameras.main.startFollow(this.rocket);
+
         this.scoreboard = new Scoreboard(this.data, this);
 
         // TODO Scalemanger should handle scaling in the future
 
         this.asteroids = new Asteroids(this.physics.world, this);
         this.stars = new Stars(this.physics.world, this);
-
-        const atlasTexture = this.textures.get('rocket');
-        const frames = atlasTexture.getFrameNames();
-        console.log(frames);
-        this.rocket = new Rocket(this, 50, 300, 'rocket', frames[0]);
+        this.stars.spawnIn();
 
         // create colliders
         const rocketAsteroidCollider = new RocketAsteroidsCollider(this,
@@ -72,10 +76,18 @@ export default class Game extends Phaser.Scene {
      * Updates the game
      */
     update() {
-        this.physics.world.wrap(this.asteroids);
-        this.physics.world.wrap(this.rocket);
-        this.rocket.move();
+        // center the world bounds around the rocket
+        Phaser.Geom.Rectangle.CenterOn(this.physics.world.bounds,
+            this.rocket.x, this.rocket.y);
 
+        // make asteroids appear again if they go out of bounds
+        this.physics.world.wrap(this.asteroids);
+
+        // make background moving reverse to the camera
+        this.universe.tilePositionX = - this.cameras.main.x;
+        this.universe.tilePositionY = - this.cameras.main.y;
+
+        this.rocket.move();
         this.asteroids.updateOnWorldBounds(this.physics.world.bounds);
     }
 }
